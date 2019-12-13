73a2613c1d29b3af4f9cfc9335c65d6f106f3f6a
jasmeet21 <jasmeetsingh21@hotmail.com>
2019-12-13 22:08:33 +0800

    Updates

---
+++ precache-manifest.6aa87c09914bcd8fb99072f31641e0c9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f56454085d5d83d9f041044d2bff5dc6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c1fbd1833c0da3987fe9",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "6f02abb9e17da1904fb6",
+    "url": "/static/js/2.8aa04f40.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.8aa04f40.chunk.js.LICENSE"
+  },
+  {
+    "revision": "c1fbd1833c0da3987fe9",
+    "url": "/static/js/main.88f47455.chunk.js"
+  },
+  {
+    "revision": "d81fdc523862c916f094",
+    "url": "/static/js/runtime-main.50d3e424.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
